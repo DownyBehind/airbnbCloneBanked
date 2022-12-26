0c3d9712ab93a7bedf124dc7f0212cee57b300b0
TeamBehindDowny@gmail.com <kim1023123@gmail.com>
2022-12-26 21:50:07 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ import environ
 
 from pathlib import Path
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY= env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
